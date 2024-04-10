ca690a30cea0a2978d7276e751805baa5e06419c
heeyil <125425138+heeyil@users.noreply.github.com>
2024-04-10 09:59:36 +0800

    Delete pythonProject2 directory

--- pythonProject2/utils/data/dataset.py
+++
@@ -1,9 +0,0 @@
-class Dataset:
-    def __init__(self):
-        pass
-
-    def __getitem__(self, index):
-        raise NotImplementedError
-
-    def __len__(self):
-        raise NotImplementedError
