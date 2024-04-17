a3e06e010d3a91c16ae2accb518c3d4587647a4c
heeyil <125425138+heeyil@users.noreply.github.com>
2024-04-17 21:28:23 +0800

    Delete DeepLearning directory

--- DeepLearning/utils/math.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-
-# softmax运算
-def softmax(x, axis=-1):
-    x_max = np.max(x, axis=axis, keepdims=True)
-    exps = np.exp(x - x_max)
-    return exps / np.sum(exps, axis=axis, keepdims=True)
